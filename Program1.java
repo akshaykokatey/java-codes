cf970a8111f23452338a007176a2f3789e547753
akshaykokatey <96278403+akshaykokatey@users.noreply.github.com>
2021-12-17 10:33:14 +0530

    Oop with java codes

---
+++ Program1.java
@@ -0,0 +1,8 @@
+public class Program1 {
+    public static void main(String args[]) {
+     
+      System.out.println("Welcome to Java,");
+      System.out.println("Learning Java Now and Programming is fun.");
+        
+    }
+}
---
+++ Program2.java
@@ -0,0 +1,31 @@
+ class Program2
+{
+  public static void main (String args[])
+  {
+//arithemetic using Integer
+    System.out.println ("Integer arthemetic");
+    int a = 2 + 1;
+    int b = a * 3;
+    int c = b / 3;
+    int d = c - b;
+      System.out.println ("a =" + a);
+      System.out.println ("b =" + b);
+      System.out.println ("c=" + c);
+      System.out.println ("d=" + d);
+//arthemetic using double
+      System.out.println ("\n Floating point arthemetic");
+    double da = 2 + 1;
+    double db = a * 3;
+    double dc = b / 3;
+    double dd = c - b;
+      System.out.println ("da=" + da);
+      System.out.println ("db=" + db);
+      System.out.println ("dc=" + dc);
+      System.out.println ("dd=" + dd);
+
+
+
+
+  }
+
+}
