54b1d807a0fa52c084f5f39b7bf9f5eccb807b22
Akshay kokatey <96278403+akshaykokatey@users.noreply.github.com>
2021-12-27 21:36:45 +0530

    Add files via upload

---
+++ constr.java
@@ -0,0 +1,19 @@
+//6 . simple constructor program
+ class constr {
+        constr()
+        {
+                int a = 10;
+                int b = 6;
+                int c ;
+                c = a + b;
+                System.out.println("****Simple constructor****");
+                System.out.println("Total of 10 + 6 = "+c);
+                
+        }      
+        
+    public static void main(String args[]) {
+        constr obj = new constr();
+        
+    }
+ 
+}
